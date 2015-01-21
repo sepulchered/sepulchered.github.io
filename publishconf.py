707a21cdc7ede87e143c40d4e2f907acf01bbd2b
Andrey Dresvyannikov <andrey@dresvyannikov.ru>
2015-01-21 10:04:35 +0300

    changes to cache etc.

--- publishconf.py
+++ publishconf.py
@@ -20,5 +20,5 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+DISQUS_SITENAME = "andreydresvyannikovru"
+GOOGLE_ANALYTICS = "UA-38597125-3"

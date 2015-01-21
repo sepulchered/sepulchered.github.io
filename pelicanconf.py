45a2f0922f6521346e7ea76aab14af0819d05717
Andrey Dresvyannikov <andrey@dresvyannikov.ru>
2015-01-21 10:38:43 +0300

    added social links and some links for empty cv and about pages

--- pelicanconf.py
+++ pelicanconf.py
@@ -24,14 +24,15 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('About', '/about.html'),
+         ('CV', '/cv.html'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('github', 'https://github.com/sepulchered'),
+          ('twitter', 'https://twitter.com/dresvyannikov_a'),
+          ('linkedin', 'ru.linkedin.com/in/sepulchered'),
+          ('facebook', 'https://www.facebook.com/dresvyannikov.a'),
+         )
 
 DEFAULT_PAGINATION = 5
 
